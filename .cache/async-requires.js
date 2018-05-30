da25b8a3cafdda811928ef7cc4be3258024a2689
Yassine Hermellin <yassine.hermellin@gmail.com>
2018-05-30 14:08:31 +0200

    _redirects

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -2,16 +2,14 @@
 const preferDefault = m => m && m.default || m
 
 exports.components = {
-  "component---cache-dev-404-page-js": require("gatsby-module-loader?name=component---cache-dev-404-page-js!/home/saraband/Desktop/DEV/my-website/.cache/dev-404-page.js"),
-  "component---src-pages-es-index-js": require("gatsby-module-loader?name=component---src-pages-es-index-js!/home/saraband/Desktop/DEV/my-website/src/pages/es/index.js"),
-  "component---src-pages-fr-index-js": require("gatsby-module-loader?name=component---src-pages-fr-index-js!/home/saraband/Desktop/DEV/my-website/src/pages/fr/index.js"),
+  "component---src-pages-index-es-js": require("gatsby-module-loader?name=component---src-pages-index-es-js!/home/saraband/Desktop/DEV/my-website/src/pages/index.es.js"),
+  "component---src-pages-index-fr-js": require("gatsby-module-loader?name=component---src-pages-index-fr-js!/home/saraband/Desktop/DEV/my-website/src/pages/index.fr.js"),
   "component---src-pages-index-js": require("gatsby-module-loader?name=component---src-pages-index-js!/home/saraband/Desktop/DEV/my-website/src/pages/index.js")
 }
 
 exports.json = {
-  "dev-404-page.json": require("gatsby-module-loader?name=path---dev-404-page!/home/saraband/Desktop/DEV/my-website/.cache/json/dev-404-page.json"),
-  "es.json": require("gatsby-module-loader?name=path---es!/home/saraband/Desktop/DEV/my-website/.cache/json/es.json"),
-  "fr.json": require("gatsby-module-loader?name=path---fr!/home/saraband/Desktop/DEV/my-website/.cache/json/fr.json"),
+  "index-es.json": require("gatsby-module-loader?name=path---index-es!/home/saraband/Desktop/DEV/my-website/.cache/json/index-es.json"),
+  "index-fr.json": require("gatsby-module-loader?name=path---index-fr!/home/saraband/Desktop/DEV/my-website/.cache/json/index-fr.json"),
   "index.json": require("gatsby-module-loader?name=path---index!/home/saraband/Desktop/DEV/my-website/.cache/json/index.json")
 }
 
--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -7,15 +7,13 @@ exports.layouts = {
 }
 
 exports.components = {
-  "component---cache-dev-404-page-js": preferDefault(require("/home/saraband/Desktop/DEV/my-website/.cache/dev-404-page.js")),
-  "component---src-pages-es-index-js": preferDefault(require("/home/saraband/Desktop/DEV/my-website/src/pages/es/index.js")),
-  "component---src-pages-fr-index-js": preferDefault(require("/home/saraband/Desktop/DEV/my-website/src/pages/fr/index.js")),
+  "component---src-pages-index-es-js": preferDefault(require("/home/saraband/Desktop/DEV/my-website/src/pages/index.es.js")),
+  "component---src-pages-index-fr-js": preferDefault(require("/home/saraband/Desktop/DEV/my-website/src/pages/index.fr.js")),
   "component---src-pages-index-js": preferDefault(require("/home/saraband/Desktop/DEV/my-website/src/pages/index.js"))
 }
 
 exports.json = {
-  "dev-404-page.json": require("/home/saraband/Desktop/DEV/my-website/.cache/json/dev-404-page.json"),
-  "es.json": require("/home/saraband/Desktop/DEV/my-website/.cache/json/es.json"),
-  "fr.json": require("/home/saraband/Desktop/DEV/my-website/.cache/json/fr.json"),
+  "index-es.json": require("/home/saraband/Desktop/DEV/my-website/.cache/json/index-es.json"),
+  "index-fr.json": require("/home/saraband/Desktop/DEV/my-website/.cache/json/index-fr.json"),
   "index.json": require("/home/saraband/Desktop/DEV/my-website/.cache/json/index.json")
 }
