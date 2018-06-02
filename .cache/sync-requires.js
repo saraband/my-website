38b592380d169ba9896010f1d8de0a68d227172c
Saraband <yassine.hermellin@gmail.com>
2018-06-02 15:49:53 +0200

    Implemented the ProjectComponent overlay controls to navigate through projects

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -7,12 +7,14 @@ exports.layouts = {
 }
 
 exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("/home/saraband/Desktop/DEV/my-website/.cache/dev-404-page.js")),
   "component---src-pages-index-es-js": preferDefault(require("/home/saraband/Desktop/DEV/my-website/src/pages/index.es.js")),
   "component---src-pages-index-fr-js": preferDefault(require("/home/saraband/Desktop/DEV/my-website/src/pages/index.fr.js")),
   "component---src-pages-index-js": preferDefault(require("/home/saraband/Desktop/DEV/my-website/src/pages/index.js"))
 }
 
 exports.json = {
+  "dev-404-page.json": require("/home/saraband/Desktop/DEV/my-website/.cache/json/dev-404-page.json"),
   "index-es.json": require("/home/saraband/Desktop/DEV/my-website/.cache/json/index-es.json"),
   "index-fr.json": require("/home/saraband/Desktop/DEV/my-website/.cache/json/index-fr.json"),
   "index.json": require("/home/saraband/Desktop/DEV/my-website/.cache/json/index.json")
--- src/components/ProjectComponent.js
+++ src/components/ProjectComponent.js
@@ -2,9 +2,11 @@ import React from 'react'
 import { connect } from 'react-redux'
 import './ProjectComponent.scss'
 import InlineSvg from './InlineSvg'
-import LeftArrow from './leftArrow.svg'
-import RightArrow from './rightArrow.svg'
+import LeftArrow from './arrow-left-red.svg'
+import RightArrow from './arrow-right-red.svg'
 import projectsData from './ProjectsData'
+import HideLogo from './eye-hidden.svg'
+import ShowLogo from './eye-open.svg'
 import {
   setProject
 } from 'Actions/index'
@@ -12,6 +14,11 @@ import {
 class ProjectComponent extends React.Component {
   constructor(props) {
     super(props)
+
+    this.state = {
+      isMenuCollapsed: false,
+      isOverlay: true
+    }
   }
 
   render() {
@@ -22,6 +29,11 @@ class ProjectComponent extends React.Component {
     } = this.props
 
     const {
+      isMenuCollapsed,
+      isOverlay
+    } = this.state
+
+    const {
       id,
       title,
       component
@@ -29,17 +41,32 @@ class ProjectComponent extends React.Component {
 
     return(
       <div id='project-container'>{this.props.test}
-        {/*<div id='project-overlay'>
-        </div>*/}
-        <div id='project-controls'>
-          <LeftArrow
-            className='arrow-img'
-            onClick={() => previousProject(id)}
-            />
-          <RightArrow
-            className='arrow-img'
-            onClick={() => nextProject(id)}
-            />
+        {isOverlay ? <div id='project-overlay' onClick={() => this.setState({isOverlay: false})}></div> : null}
+        <div id='project-controls' className={isMenuCollapsed ? 'menuCollapse' : 'menuExpand'}>
+          <div id='project-controls-hide'>
+            {!isMenuCollapsed ? (
+              <HideLogo id='hide-logo' onClick={() => this.setState({isMenuCollapsed: true})}/>
+            ) : (
+              <ShowLogo id='hide-logo' onClick={() => this.setState({isMenuCollapsed: false})}/>
+             )}
+          </div>
+          <div id='project-controls-title'>
+            <LeftArrow
+              className='arrow-img'
+              onClick={() => previousProject(id)}
+              />
+            <div className='flex-col'>
+              <h1>Interface design & implementation</h1>
+              <h3>Findimo, real estate agency</h3>
+            </div>
+            <RightArrow
+              className='arrow-img'
+              onClick={() => nextProject(id)}
+              />
+          </div>
+          <div id='project-controls-description'>
+            <p>Création du design (Photoshop, InDesign, Illustrator).<br />Implémentation de l'interface à l'aide de ReactJS, HTML, CSS3.<br />Backend: NodeJS, Express, MongoDB.<br />Autre: Webpack</p>
+          </div>
         </div>
         <div id='project-panel' className={currentProjectFade} >
           {component}
