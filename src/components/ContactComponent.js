5bf7e60d9ecd49a8e2d80f474cb3d8300922c335
Saraband <yassine.hermellin@gmail.com>
2018-06-25 17:29:26 +0200

    deploy

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -16,7 +16,16 @@ class ContactComponent extends React.Component {
     this.state = {
       name: '',
       email: '',
-      message: ''
+      phone: '',
+      message: '',
+      sent: false,
+
+      errors: {
+        name: false,
+        email: false,
+        phone: '',
+        message: false
+      }
     }
   }
 
@@ -24,6 +33,29 @@ class ContactComponent extends React.Component {
 
   handleSubmit = (event) => {
     event.preventDefault();
+
+    const isValid = {
+      name: (v) => v && v.length > 0,
+      email: (v) => v && v.length > 0 && /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(v),
+      phone: (v) => v && v.length > 0 && /[0-9 ]+/.test(v),
+      message: (v) => v && v.length > 10,
+    }
+
+    let errors =  {};
+    for(let key in isValid) {
+      if(!isValid[key](this.state[key]))
+        errors[key] = true
+    }
+
+    this.setState({errors})
+
+    if(Object.keys(errors).length > 0) {
+      console.log(errors)
+      return
+    }
+
+    // Send form
+    this.setState({sent: true})
   }
 
   render() {
@@ -31,7 +63,10 @@ class ContactComponent extends React.Component {
     const {
       name,
       email,
-      message
+      phone,
+      message,
+      sent,
+      errors
     } = this.state
 
     return(
@@ -42,24 +77,34 @@ class ContactComponent extends React.Component {
         </h1>
         <form onSubmit={this.handleSubmit} autoComplete='off' data-test='test'>
           <Input 
+            error={errors.name}
             value={name}
             onChange={this.handleChange}
             name='name'
             placeholder={tr('contact-name-placeholder')}
             />
           <Input 
+            error={errors.email}
             value={email}
+            onChange={this.handleChange}
+            name='phone'
+            placeholder={tr('contact-phone-placeholder')}
+            />
+          <Input
+            error={errors.phone}
+            value={phone}
             onChange={this.handleChange}
             name='email'
             placeholder={tr('contact-email-placeholder')}
             />
           <Textarea 
+            error={errors.message}
             value={message}
             onChange={this.handleChange}
             name='message'
             placeholder={tr('contact-message-placeholder')}
             />
-          <Button style={{width: '200px', alignSelf: 'flex-end'}} >{tr('contact-button')}</Button>
+          <Button style={{width: '200px', alignSelf: 'flex-end'}} >{sent ? tr('contact-button-sent') : tr('contact-button')}</Button>
         {/*} animation button qui se remplit depuis le milieu */}
         </form>
       </div>
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,23 +1,23 @@
 import React from 'react'
 import s from './Form.module.scss'
 
-export const Input = ({value, onChange, name, ...props}) => (
+export const Input = ({value, onChange, name, error, ...props}) => (
   <div className={s.container} data-aos='fade-right'>
-    <input type='text' value={value} autoComplete='off' onChange={onChange} name={name} {...props} />
+    <input type='text' value={value} autoComplete='off' className={error ? s.error : null} onChange={onChange} name={name} {...props} />
     <div className={s.border}></div>
   </div>
 )
 
-export const Textarea = ({value, onChange, name, ...props}) => (
+export const Textarea = ({value, onChange, name, error, ...props}) => (
   <div className={s.container} data-aos='fade-right'>
-    <textarea value={value} onChange={onChange} name={name} {...props} />
+    <textarea value={value} onChange={onChange} name={name} className={error ? s.error : null} {...props} />
     <div className={s.border}></div>
   </div>
 )
 
-export const Button = ({children, ...props}) => (
+export const Button = ({children, disabled, ...props}) => (
   <div className={s.container} {...props} data-aos='fade-left'>
-    <button>{children}</button>
+    <button disabled={disabled} >{children}</button>
     <div className={s.border}></div>
   </div>
 )
